c0faea59e7ca1f36a166e847524cffc8deac702f
Browser <92232900+browse999@users.noreply.github.com>
2024-08-12 19:45:00 +0300

    Add files via upload

---
+++ sayilari_kiyasla.js
@@ -0,0 +1,21 @@
+
+//  İki sayıyı kıyaslayan program
+
+sayi1 = Number(prompt('İlk sayiyi giriniz'))
+
+sayi2 = Number(prompt('İkinci sayıyı giriniz'))
+
+
+if(sayi1>sayi2) {
+
+    alert('Sayi 1 Sayi 2 den büyüktür')
+}
+
+else if(sayi2>sayi1) {
+
+    alert('Sayi 2 Sayi 1 Den büyüktür')
+}
+
+else {
+    alert('hata lütfen tekrar deneyiniz')
+}
